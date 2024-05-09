8ac419e9176f9f5026305b341301db080433fb42
thasup <thanachonfirst@hotmail.com>
2024-05-10 04:40:19 +0700

    chore: Install dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  pages: true,
+  modules: [
+    'nuxt-icon',
+    '@nuxtjs/tailwindcss',
+    // '@nuxtjs/supabase',
+    '@pinia/nuxt',
+  ],
+  runtimeConfig: {
+    public: {
+      bucketUrl: process.env.BUCKET_URL,
+    }
+  },
   devtools: { enabled: true }
 })
